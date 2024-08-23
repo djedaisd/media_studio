b690921786ced06ae3d77a245a08e090a27ece02
djedaisd <dnuric@bk.ru>
2024-08-24 00:27:45 +0500

    fixed bugs

--- src/components/Team.js
+++ src/components/Team.js
@@ -3,62 +3,88 @@ import '../index.css'; // Импорт вашего CSS файла
 import team1 from '../assets/team1.jpg';
 import team2 from '../assets/team2.jpg';
 import yergen from '../assets/yerda.jpg';
+import nurik from '../assets/nurik.jpg';
+import bibo from '../assets/bibosnv.jpg';
+import logosev from '../assets/losavage.jpg';
+
+
+
+const teamMembers = [
+  {
+    imgSrc: team1,
+    name: 'YRYSBEK ADILBEKULY',
+    position: 'ВИДЕОГРАФ',
+  },
+  {
+    imgSrc: team2,
+    name: 'SAGAT AKIMBAI',
+    position: 'VFX,VR специалист',
+  },
+  {
+    imgSrc: yergen,
+    name: 'DAMIR YERDAULET',
+    position: 'IT Product Manager',
+  },
+  ,
+  {
+    imgSrc: bibo,
+    name: 'ERALY',
+    position: 'Marketer',
+  },
+  {
+    imgSrc: nurik,
+    name: 'SEIDASHIM NURZHIGIT',
+    position: 'Front-End Developer',
+  },
+  ,
+
+  {
+    imgSrc: logosev,
+    name: 'ADIL',
+    position: 'Marketer',
+  },
+  // Добавьте сюда новые объекты для новых сотрудников
+];
+
 function Teams() {
   return (
     <div>
-    <div class="service-header">
+      <div className="service-header"  id="locations">
         <div>
-            <h2>КОМАНДА</h2>
-
+          <h2>КОМАНДА</h2>
         </div>
-        <div class="team-book">MEDIA STUDIO</div>
-
-    </div>
-    <div>
-        <div class="header_text4">
-            Мы предлагаем высококачественные
-            оборудования, помощь профессионалов и
-            разные локации для вашего удобства!
-            Качественная акустика, кондиционер,
-            чистота студии и  приятное освещение
-            сделает ваш опыт в нашей студии самым
-            приятным и продуктивным!
-
-            Забронируйте время для съемок
-            прямо сейчас!
-
-        </div>
-        <div class="header_text5">
-            Забронируйте время для съемок
-            прямо сейчас!
-        </div>
-
-    </div>
-    <div className="row-img" id='locations'>
-      <div className="withtext">
-        <div className="circular_image">
-          <img src={team1} alt="Team Member 1" />
-        </div>
-        <h1>YRYSBEK ADILBEKULY</h1>
-        <p>ВИДЕОГРАФ</p>
+        <div className="team-book">MEDIA STUDIO</div>
       </div>
 
-      <div className="withtext">
-        <div className="circular_image">
-          <img src={team2} alt="Team Member 2" />
-        </div>
-        <h1>SAGAT AKIMBAI</h1>
-        <p>VFX,VR специалист</p>
+      <div className="header_text4">
+        Мы предлагаем высококачественные
+        оборудования, помощь профессионалов и
+        разные локации для вашего удобства!
+        Качественная акустика, кондиционер,
+        чистота студии и приятное освещение
+        сделает ваш опыт в нашей студии самым
+        приятным и продуктивным!
+
+        Забронируйте время для съемок
+        прямо сейчас!
       </div>
 
-      <div className="withtext">
-        <div className="circular_image">
-          <img src={yergen} alt="Team Member 3" />
-        </div>
-        <h1>DAMIR YERDAULET</h1>
-        <p>IT Product Manager</p>
+      <div className="header_text5">
+        Забронируйте время для съемок
+        прямо сейчас!
       </div>
-    </div>
+
+      <div className="team-container">
+        {teamMembers.map((member, index) => (
+          <div className="team-member" key={index}>
+            <div className="circular_image">
+              <img src={member.imgSrc} alt={`Team Member ${index + 1}`} />
+            </div>
+            <h1>{member.name}</h1>
+            <p>{member.position}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
