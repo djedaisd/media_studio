09b8e6a2cbee72be315c3f72435db2a100176584
djedaisd <dnuric@bk.ru>
2024-08-25 22:49:16 +0500

    updated reqs

--- src/submit.py
+++ src/submit.py
@@ -58,6 +58,6 @@ def submit():
     else:
         return {"status": "failure", "message": result.text}, 500
 
-if __name__ == '_main_':
+if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
